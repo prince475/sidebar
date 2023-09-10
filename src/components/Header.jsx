272aeb2763ba99f80b1e12b44b686af9e8d6bf12
prince475 <danielmwai475@gmail.com>
2023-09-10 23:55:55 +0300

    Installing material Ui to this project

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function  Header() {
+  return (
+    <div className='header'>
+        <MenuIcon />
+        <h1>I am a Header</h1>
+
+    </div>
+  )
+}
